571894b4ab2ace96558273f4b09dd7dbd572d17c
JeffersonMat <jeff_08062011@hotmail.com>
2020-08-28 12:40:27 +1200

    modeling space for videos in portfolio component

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -9,27 +9,59 @@ const Portfolio = () => {
       <div className="carousel-item yellow white-text" href="#one!">
         <h2>Projects I have worked on</h2>
         <p className="white-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-          Repudiandae, at. Magni molestias neque laborum unde reiciendis
-          illo consectetur blanditiis, suscipit sequi quasi possimus.
-          Ipsum voluptatibus deleniti a animi esse veniam.
-          </p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+          at. Magni molestias neque laborum unde reiciendis illo consectetur
+          blanditiis, suscipit sequi quasi possimus. Ipsum voluptatibus deleniti
+          a animi esse veniam.
+        </p>
       </div>
       <div className="carousel-item red white-text" href="#one!">
         <h2>First Panel</h2>
         <p className="white-text">This is your first panel</p>
+        <div className="item-1">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+            facilis! Consequatur, libero possimus expedita blanditiis quod,
+            quidem ratione recusandae in delectus voluptate unde consectetur
+            tempore exercitationem officiis accusantium dolor aliquam.
+          </p>
+        </div>
       </div>
       <div className="carousel-item amber white-text" href="#two!">
         <h2>Second Panel</h2>
         <p className="white-text">This is your second panel</p>
+        <div className="item-2">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+            facilis! Consequatur, libero possimus expedita blanditiis quod,
+            quidem ratione recusandae in delectus voluptate unde consectetur
+            tempore exercitationem officiis accusantium dolor aliquam.
+          </p>
+        </div>
       </div>
       <div className="carousel-item green white-text" href="#three!">
         <h2>Third Panel</h2>
         <p className="white-text">This is your third panel</p>
+        <div className="item-3">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+            facilis! Consequatur, libero possimus expedita blanditiis quod,
+            quidem ratione recusandae in delectus voluptate unde consectetur
+            tempore exercitationem officiis accusantium dolor aliquam.
+          </p>
+        </div>
       </div>
       <div className="carousel-item blue white-text" href="#four!">
         <h2>Fourth Panel</h2>
         <p className="white-text">This is your fourth panel</p>
+        <div className="item-4">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+            facilis! Consequatur, libero possimus expedita blanditiis quod,
+            quidem ratione recusandae in delectus voluptate unde consectetur
+            tempore exercitationem officiis accusantium dolor aliquam.
+          </p>
+        </div>
       </div>
     </div>
   )
