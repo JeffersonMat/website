15d28c66c5b4c3cff3c190f2ad770f4efaf530cc
JeffersonMat <jeff_08062011@hotmail.com>
2020-09-01 12:32:06 +1200

    fix landscape 667px

--- client/components/WhoIam.jsx
+++ client/components/WhoIam.jsx
@@ -6,12 +6,11 @@ const WhoIam = () => {
     return (
       <>
         <section className="who-i-am-background"></section>
-         <section id="who-i-am">
+        <section id="who-i-am">
           <div className=" carousel carousel-slider  who-i-am">
-              
             <div className="carousel-item" href="#one!">
               <div className="content-who-i-am">
-                <h1>Who I am...</h1>
+                <h1 className="who-i-am-title">Who I am...</h1>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Dicta, aut tempore. Animi, est eum alias dignissimos ullam
@@ -31,34 +30,33 @@ const WhoIam = () => {
                   nihil vero voluptatibus itaque commodi libero a error officiis
                   soluta facere accusantium reiciendis?
                 </p>
-                </div>
               </div>
-         
+            </div>
+
             <div className="carousel-item " href="#two!">
-               <div className="content-who-i-am">
-              <h1>What I Love...</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
-                aut tempore. Animi, est eum alias dignissimos ullam nihil vero
-                voluptatibus itaque commodi libero a error officiis soluta
-                facere accusantium reiciendis?
-              </p>
+              <div className="content-who-i-am">
+                <h1 className="who-i-am-title">What I Love...</h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dicta, aut tempore. Animi, est eum alias dignissimos ullam
+                  nihil vero voluptatibus itaque commodi libero a error officiis
+                  soluta facere accusantium reiciendis?
+                </p>
 
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
-                aut tempore. Animi, est eum alias dignissimos ullam nihil vero
-                voluptatibus itaque commodi libero a error officiis soluta
-                facere accusantium reiciendis?
-              </p>
-              <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
-                  aut tempore. Animi, est eum alias dignissimos ullam nihil vero
-                  voluptatibus itaque commodi libero a error officiis soluta
-                  facere accusantium reiciendis?
-              </p>
-                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dicta, aut tempore. Animi, est eum alias dignissimos ullam
+                  nihil vero voluptatibus itaque commodi libero a error officiis
+                  soluta facere accusantium reiciendis?
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dicta, aut tempore. Animi, est eum alias dignissimos ullam
+                  nihil vero voluptatibus itaque commodi libero a error officiis
+                  soluta facere accusantium reiciendis?
+                </p>
               </div>
-             
+            </div>
           </div>
         </section>
       </>
