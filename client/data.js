5cfba0b87ff2f0876fa163100a498b3ba5a3653f
JeffersonMat <jeff_08062011@hotmail.com>
2020-09-06 11:22:32 +1200

    add task manager video and change content

--- client/data.js
+++ client/data.js
@@ -1,15 +1,15 @@
 module.exports = [
   {
     title: "Pantree",
-    description: "Group Project at EDA. Full-Stack Web-App. Meal app planner.",
+    description:
+      "Group Project at EDA. Full-Stack Web-App using CRUD functionality.  React, Redux, Express, Node.js. Meal app planner.",
     video: "../../videos/Pantree.mp4",
     src: "https://pantree-app.herokuapp.com/#/",
   },
 
   {
     title: "Life of the Party",
-    description:
-      "Group Project at EDA. Full-Stack Web-App. Drinking game.",
+    description: "Group Project at EDA. Full-Stack Web-App. Drinking game.",
     video: "../../videos/L.O.T.P.mp4",
     src: "https://life-of-the-party.herokuapp.com",
   },
@@ -22,31 +22,29 @@ module.exports = [
   // },
   {
     title: "Mind Eater",
-    description:
-      "Group Project at EDA. - HandleBars, Full-Stack. Game.",
+    description: "Group Project at EDA. - HandleBars, Full-Stack. Game.",
 
     video: "../../videos/mind-eater.mp4",
     src: "http://mind-eater.herokuapp.com/",
   },
   {
     title: "Pong",
-    description:
-      "Group Poject at EDA. -Vanilla js-. Pong Game",
+    description: "Group Poject at EDA. -Vanilla js-. Pong Game",
     video: "../videos/pong.mp4",
     src: "",
   },
   {
     title: "QuizzApp",
     description:
-      "Personal Project. -Vue js-. Quiz Web-App. Quiz night app, why not?",
+      "Personal Project as part of my self lead learning Vue.js. Using external API. Quiz Web-App. Quiz night app, why not?",
     video: "../../videos/QuizzApp.mp4",
     src: "",
   },
-  // {
-  //   title: "Task Manager",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, aut tempore. Animi, est eum alias dignissimos ullamnihil vero voluptatibus itaque commodi libero a error officiis soluta facere accusantium reiciendis?",
-  //   video: "../viewwe",
-  //   src: "",
-  // },
+  {
+    title: "Task Manager",
+    description:
+      "Personal Project at EDA. -Full-Stack Web App using CRUD functionality. React, Redux, Express, Node.js. Task Manager.",
+    video: "../../videos/taskmanager.mp4",
+    src: "http://taskss-manager.herokuapp.com/",
+  },
 ]
