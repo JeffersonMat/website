5ba286c08ea4c15172ebbe3d28bad7d229bf82e7
JeffersonMat <jeff_08062011@hotmail.com>
2020-09-09 15:18:23 +1200

    add transitions

--- client/components/WhoIam.jsx
+++ client/components/WhoIam.jsx
@@ -4,11 +4,41 @@ import Banner from './banner/Banner'
 
 class  WhoIam extends React.Component {
   
+  state = {
+    selected: false,
+    slide: false,
+    index:0
+    
+  }
   
 
-  handleClick() {
-    const indicator = document.querySelectorAll('.control li')
-    const indicatorParent = document.querySelector('.control ul')
+  handleClickNext=() => {
+ 
+    this.setState({
+    selected: false,
+      slider: true,
+      index: 1
+   }) 
+  }
+  
+  handleClickPrev = () => {
+        this.setState({
+          selected: false,
+          slider: true,
+          index: 0,
+        }) 
+  }
+
+   slideStyling = (index) => {
+ 
+     switch (index) {
+     
+       case 0:
+         return {transform: 'translateX(' + (index) * -50 + '%)'}
+     
+       case 1:
+         return {transform: 'translateX(' + (index) * -50 + '%)'} 
+    }
   }
   
   
@@ -22,7 +52,10 @@ class  WhoIam extends React.Component {
         <section id="who-i-am">
           <div className="container">
             <div className=" carousel-who-i-am  who-i-am">
-              <div className="slider-who-i-am" href="#one!">
+              <div
+                className="slider-who-i-am"
+                style={this.slideStyling(this.state.index)}
+              >
                 <ScrollAnimation animateIn="fadeInUp" duration={1.2}>
                   <div id="content-who-i-am">
                     <div>
@@ -30,25 +63,25 @@ class  WhoIam extends React.Component {
                     </div>
                     <div>
                       <p>
-                        A Colombian with an intrepid spirit and a creative mind. I
-                        believe technology has the potential to transform our world
-                        for the better. I do what I do because I love using
-                        technology to solve problems and do good.
-                  </p>
+                        A Colombian with an intrepid spirit and a creative mind.
+                        I believe technology has the potential to transform our
+                        world for the better. I do what I do because I love
+                        using technology to solve problems and do good.
+                      </p>
 
                       <p>
-                        People describe me as a go-getter. I’m not afraid to travel
-                        off the beaten track if that means a breakthrough is just
-                        around the corner. I’m an innovator, a creator, and a
-                        lifelong learner. And I make a mean empanada.
-                  </p>
+                        People describe me as a go-getter. I’m not afraid to
+                        travel off the beaten track if that means a breakthrough
+                        is just around the corner. I’m an innovator, a creator,
+                        and a lifelong learner. And I make a mean empanada.
+                      </p>
 
                       <p>
                         When it comes to life, my motto is simple; never stop
                         learning. It was this enthusiasm for learning and growth
-                        that lead me to tech—an industry of constant innovation and
-                        adaptability.
-                  </p>
+                        that lead me to tech—an industry of constant innovation
+                        and adaptability.
+                      </p>
                     </div>
                   </div>
                 </ScrollAnimation>
@@ -58,41 +91,57 @@ class  WhoIam extends React.Component {
                       <h1 className="who-i-am-title">A few things about me.</h1>
                     </div>
                     <p>
-                      I call Lyall Bay home. I love a fresh hop IPA. I once climbed
-                      the highest peak in the Colombian West Andes. I support
-                      Manchester United. Don’t talk to me about rugby; football and
-                      rugby are no comparison.
-                </p>
+                      I call Lyall Bay home. I love a fresh hop IPA. I once
+                      climbed the highest peak in the Colombian West Andes. I
+                      support Manchester United. Don’t talk to me about rugby;
+                      football and rugby are no comparison.
+                    </p>
 
                     <p>
-                      I love taking photos. I once had a chocolate labrador called
-                      Coffee. I live with my Kiwi partner and fiancé. I don’t like
-                      cricket. Mountains over beach. Camping over hotels. Hiking
-                      over swimming.
-                </p>
+                      I love taking photos. I once had a chocolate labrador
+                      called Coffee. I live with my Kiwi partner and fiancé. I
+                      don’t like cricket. Mountains over beach. Camping over
+                      hotels. Hiking over swimming.
+                    </p>
 
                     <p>
-                      I’ve seen The Matrix nine times. I love black coffee. I play
-                      FIFA like a boss. I box. My favourite food is bacon. I can
-                      quote all the lyrics to Pink Floyd’s Pulse album. I’m a
-                      terrible singer.
-                </p>
+                      I’ve seen The Matrix nine times. I love black coffee. I
+                      play FIFA like a boss. I box. My favourite food is bacon.
+                      I can quote all the lyrics to Pink Floyd’s Pulse album.
+                      I’m a terrible singer.
+                    </p>
                   </div>
                 </ScrollAnimation>
-                <div className="control">
-                  <span className="arrow left">
-                    <i className="material-icons" onClick={}>keyboard_arrow_left</i>
-                  </span>
-                  <span className="arrow right">
-                    <i className="material-icons" onClick={}>keyboard_arrow_right</i>
-                  </span>
-                  <ul>
-                    <li className="selected"></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                  </ul>
-                </div>
+              </div>
+              <div className="control">
+                <span className="arrow left">
+                  <i
+                    className="material-icons"
+                    onClick={
+                      this.state.index == 0
+                        ? this.handleClickNext
+                        : this.handleClickPrev
+                    }
+                  >
+                    keyboard_arrow_left
+                  </i>
+                </span>
+                <span className="arrow right">
+                  <i
+                    className="material-icons"
+                    onClick={
+                      this.state.index == 0
+                        ? this.handleClickNext
+                        : this.handleClickPrev
+                    }
+                  >
+                    keyboard_arrow_right
+                  </i>
+                </span>
+                <ul>
+                  <li className="selected"></li>
+                  <li></li>
+                </ul>
               </div>
             </div>
           </div>
