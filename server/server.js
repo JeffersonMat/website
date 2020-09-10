8ab239825d4c56fdcc797f5c831ed9b747a82a42
JeffersonMat <jeff_08062011@hotmail.com>
2020-09-10 15:53:14 +1200

    add compression middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const express = require('express')
-const compression = require('compression-webpack-plugin')
+const compression = require('compression')
 
 
 const server = express()
 
-server.use(compression())
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
-
+server.use(compression())
 
 module.exports = server
